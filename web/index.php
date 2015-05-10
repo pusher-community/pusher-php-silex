19ba8d7b62ddb6b7118cdc6da099abc4ef332f94
leggetter <phil@leggetter.co.uk>
2015-05-10 12:38:59 +0100

    adding logging

--- web/index.php
+++ web/index.php
@@ -5,12 +5,6 @@ require('../vendor/autoload.php');
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$pusher_app_id = getenv('PUSHER_APP_ID');
-$pusher_app_key = getenv('PUSHER_APP_KEY');
-$pusher_app_secret = getenv('PUSHER_APP_SECRET');
-
-$pusher = new Pusher($pusher_app_key, $pusher_app_secret, $pusher_app_id);
-
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -23,6 +17,29 @@ $app->register(new JDesrosiers\Silex\Provider\CorsServiceProvider(), array(
     "cors.allowOrigin" => "*",
 ));
 
+// Pusher
+$pusher_app_id = getenv('PUSHER_APP_ID');
+$pusher_app_key = getenv('PUSHER_APP_KEY');
+$pusher_app_secret = getenv('PUSHER_APP_SECRET');
+
+$pusher = new Pusher($pusher_app_key, $pusher_app_secret, $pusher_app_id);
+
+// Pusher Logging
+class PusherMonoLogger {
+  function __construct($monolog) {
+    $this->monolog = $monolog;
+  }
+  
+  public function log( $msg ) {
+    $this->monolog->addDebug($msg);
+  }
+}
+
+$pusherMonoLogger = new PusherMonoLogger( $app['monolog'] );
+$pusher->set_logger( $pusherMonoLogger );
+$pusherMonoLogger->log($pusher);
+
+// Routes
 $app->get('/', function() {
   return new Response('Pusher PHP Auth Test', 200);
 });
