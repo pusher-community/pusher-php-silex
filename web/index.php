1b77af1b55eb8792b345c5e211305a06eff893a6
leggetter <phil@leggetter.co.uk>
2015-05-10 12:15:58 +0100

    making Pusher PHP auth endpoint

--- web/index.php
+++ web/index.php
@@ -2,6 +2,15 @@
 
 require('../vendor/autoload.php');
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$pusher_app_id = getenv('PUSHER_APP_ID');
+$pusher_app_key = getenv('PUSHER_APP_KEY');
+$pusher_app_secret = getenv('PUSHER_APP_SECRET');
+
+$pusher = new Pusher($pusher_app_key, $pusher_app_secret, $pusher_app_id);
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -10,11 +19,15 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+$app->post('/', function (Request $request) {
+  $channel_name = $request->get('channel_name');
+  $socket_id = $request->get('socket_id');
+
+  $auth = $pusher->socket_auth($channel_name, $socket_id);
+
+  $jsonResponse = $app->json($auth, 200);
+  return $jsonResponse;
 });
 
 $app->run();
